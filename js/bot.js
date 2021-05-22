ce0582c54f813658119c066b6a16c1365a9e96f0
Oak <ozaaaaa@gmail.com>
2021-05-22 19:30:26 +0700

    update

--- js/bot.js
+++ js/bot.js
@@ -4,6 +4,10 @@ class bot{
     this.isBotRunning = false;
     this.alertCaptcha = false;
     this.checkCpuPercent = 90;
+    this.timerDelay = 810000;
+    this.checkMinedelay = false;
+    this.firstMine = true;
+    this.previousMineDone = false;
 }
 
 delay = (millis) =>
@@ -31,7 +35,6 @@ async postData(url = '', data = {}) {
 
 async checkCPU (userAccount){
   let result = true;
-
   while(result){
     try {
       const accountDetail = await this.postData('https://api.waxsweden.org/v1/chain/get_account', { account_name: userAccount })
@@ -64,6 +67,23 @@ appendMessage(msg , box = ''){
   boxMessage.scrollTop = boxMessage.scrollHeight;
 }
 
+countDown(countDown){
+  var countDownDisplay = countDown/1000;
+  const x = setInterval(function() {
+    document.getElementById("text-cooldown").innerHTML = countDownDisplay + " Sec"
+    countDown = countDown - 1000;
+    countDownDisplay = countDown/1000;
+    if (countDown < 0) {
+      clearInterval(x);
+      document.getElementById("text-cooldown").innerHTML = "Go mine";
+    }
+  }, 1000);
+}
+
+async getUserMineDelay(userAccount){
+  return getMineDelay(userAccount);
+}
+
 async stop() {
   this.isBotRunning = false;
   this.appendMessage("bot STOP")
@@ -71,124 +91,135 @@ async stop() {
 }
 
 async start() {
-const userAccount = await wax.login();
-unityInstance.SendMessage(
-  "Controller",
-  "Server_Response_LoginData",
-  userAccount
-);
-this.isBotRunning = true;
-await this.delay(2000);
-console.log("bot StartBot");
-this.appendMessage("bot START")
-let checkMinedelay = false
-while (this.isBotRunning) {
-  let firstMine = true;
-  let previousMineDone = false;
-  let minedelay = 1;
-  do {
-    // minedelay = await getMineDelay(userAccount);
-    if(checkMinedelay){
-      minedelay = 810000;
-  }   
-    console.log(`%c[Bot] Cooldown for ${Math.ceil((minedelay / 1000)/60)} min`, 'color:green');
-    this.appendMessage(`Cooldown for ${Math.ceil((minedelay / 1000)/60)} min`)
-    await this.delay(minedelay + Math.floor(1000 + (Math.random() * 9000)));
-    minedelay = 0;
-    console.log("bot checkCPU1");
-    await this.checkCPU(userAccount);
-  } while (minedelay !== 0 && (previousMineDone || firstMine));
-  const balance = await getBalance(userAccount, wax.api.rpc);
-  console.log(`%c[Bot] balance: (before mine) ${balance}`, 'color:green');
-  
-  const mine_work = await background_mine(userAccount);
+  const userAccount = await wax.login();
+  document.getElementById("text-user").innerHTML = userAccount
+  console.log('timerDelay',this.timerDelay,'checkCpuPercent',this.checkCpuPercent)
   unityInstance.SendMessage(
     "Controller",
-    "Server_Response_Mine",
-    JSON.stringify(mine_work)
+    "Server_Response_LoginData",
+    userAccount
   );
-
-  const mine_data = {
-    miner: mine_work.account,
-    nonce: mine_work.rand_str,
-  };
-  const actions = [
-    {
-      account: mining_account,
-      name: "mine",
-      authorization: [
-        {
-          actor: mine_work.account,
-          permission: "active",
-        },
-      ],
-      data: mine_data,
-    },
-  ];
-  
-  try {
-    console.log("bot checkCPU2");
-    await this.checkCPU(userAccount);
-    if(this.alertCaptcha){
-      const audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
-      audio.play();
-    }
-
-    const result = await wax.api.transact(
-      {
-        actions,
-      },
-      {
-        blocksBehind: 3,
-        expireSeconds: 360,
-      }
-    );
-    console.log(`%c[Bot] result is = ${result}`, 'color:green');
-    var amounts = new Map();
-    if (result && result.processed) {
-      result.processed.action_traces[0].inline_traces.forEach((t) => {
-        if (t.act.data.quantity) {
-          const mine_amount = t.act.data.quantity;
-          console.log(`%c[Bot] ${mine_work.account} Mined ${mine_amount}`, 'color:green');
-          if (amounts.has(t.act.data.to)) {
-            let obStr = amounts.get(t.act.data.to);
-            obStr = obStr.substring(0, obStr.length - 4);
-            let nbStr = t.act.data.quantity;
-            nbStr = nbStr.substring(0, nbStr.length - 4);
-            let balance = (parseFloat(obStr) + parseFloat(nbStr)).toFixed(4);
-            amounts.set(t.act.data.to, balance.toString() + " TLM");
-          } else {
-            amounts.set(t.act.data.to, t.act.data.quantity);
-          }
+  this.isBotRunning = true;
+  await this.delay(2000);
+  console.log("bot StartBot");
+  this.appendMessage("bot START")
+  while (this.isBotRunning) {
+    let minedelay = 1;
+    do {
+      if(this.timerDelay != 0){
+        if(this.checkMinedelay){
+          minedelay = this.timerDelay;
         }
-      });
-      unityInstance.SendMessage(
-        "Controller",
-        "Server_Response_Claim",
-        amounts.get(mine_work.account)
-      );
-      this.appendMessage(amounts.get(mine_work.account) + " TLM",'2')
-      firstMine = false;
-      previousMineDone = true;
-      checkMinedelay = true;
-    }
-  } catch (err) {
-    unityInstance.SendMessage(
-      "ErrorHandler",
-      "Server_Response_SetErrorData",
-      err.message
-    );
-    previousMineDone = false;
-    checkMinedelay = false;
-    console.log(`%c[Bot] Error:${err.message}`, 'color:red');
-    this.appendMessage(`Error:${err.message}`)
+      }else{
+        minedelay = await getMineDelay(userAccount);
+      }
+      // console.log(`%c[Bot] Cooldown for ${Math.ceil((minedelay / 1000)/60)} min`, 'color:green');
+      this.countDown(minedelay)
+      const RandomTimeWait = minedelay + Math.floor(1000 + (Math.random() * 9000))
+      this.appendMessage(`Cooldown for ${Math.ceil((RandomTimeWait / 1000)/60)} min`)
+      await this.delay(RandomTimeWait);
+      minedelay = 0;
+      console.log("bot checkCPU1");
+      await this.checkCPU(userAccount);
+      console.log(minedelay !== 0 && (this.previousMineDone || this.firstMine),minedelay,this.previousMineDone,this.firstMine)
+    } while (minedelay !== 0 && (this.previousMineDone || this.firstMine));
+    await this.mine(userAccount)
   }
-
-  const afterMindedBalance = await getBalance(userAccount, wax.api.rpc);
-  this.appendMessage(`balance (after mined): ${afterMindedBalance}`)
-  document.getElementById("text-balance").value = afterMindedBalance
-  // console.log(`%c[Bot] balance (after mined): ${afterMindedBalance}`, 'color:green');
 }
+
+async mine(userAccount){
+  document.getElementById("btn-mine").disabled = true
+  const balance = await getBalance(userAccount, wax.api.rpc);
+    console.log(`%c[Bot] balance: (before mine) ${balance}`, 'color:green');
+    
+    const mine_work = await background_mine(userAccount);
+    unityInstance.SendMessage(
+      "Controller",
+      "Server_Response_Mine",
+      JSON.stringify(mine_work)
+    );
+
+    const mine_data = {
+      miner: mine_work.account,
+      nonce: mine_work.rand_str,
+    };
+    const actions = [
+      {
+        account: mining_account,
+        name: "mine",
+        authorization: [
+          {
+            actor: mine_work.account,
+            permission: "active",
+          },
+        ],
+        data: mine_data,
+      },
+    ];
+    
+    try {
+      console.log("bot checkCPU2");
+      await this.checkCPU(userAccount);
+      if(this.alertCaptcha){
+        const audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
+        audio.play();
+      }
+
+      const result = await wax.api.transact(
+        {
+          actions,
+        },
+        {
+          blocksBehind: 3,
+          expireSeconds: 360,
+        }
+      );
+      console.log(`%c[Bot] result is = ${result}`, 'color:green');
+      var amounts = new Map();
+      if (result && result.processed) {
+        result.processed.action_traces[0].inline_traces.forEach((t) => {
+          if (t.act.data.quantity) {
+            const mine_amount = t.act.data.quantity;
+            console.log(`%c[Bot] ${mine_work.account} Mined ${mine_amount}`, 'color:green');
+            if (amounts.has(t.act.data.to)) {
+              let obStr = amounts.get(t.act.data.to);
+              obStr = obStr.substring(0, obStr.length - 4);
+              let nbStr = t.act.data.quantity;
+              nbStr = nbStr.substring(0, nbStr.length - 4);
+              let balance = (parseFloat(obStr) + parseFloat(nbStr)).toFixed(4);
+              amounts.set(t.act.data.to, balance.toString() + " TLM");
+            } else {
+              amounts.set(t.act.data.to, t.act.data.quantity);
+            }
+          }
+        });
+        unityInstance.SendMessage(
+          "Controller",
+          "Server_Response_Claim",
+          amounts.get(mine_work.account)
+        );
+        this.appendMessage(amounts.get(mine_work.account) + " TLM",'2')
+        this.firstMine = false;
+        this.previousMineDone = true;
+        this.checkMinedelay = true;
+      }
+    } catch (err) {
+      unityInstance.SendMessage(
+        "ErrorHandler",
+        "Server_Response_SetErrorData",
+        err.message
+      );
+      this.previousMineDone = false;
+      this.checkMinedelay = false;
+      console.log(`%c[Bot] Error:${err.message}`, 'color:red');
+      this.appendMessage(`Error:${err.message}`)
+    }
+
+    const afterMindedBalance = await getBalance(userAccount, wax.api.rpc);
+    this.appendMessage(`balance (after mined): ${afterMindedBalance}`)
+    document.getElementById("text-balance").innerHTML = afterMindedBalance
+    // console.log(`%c[Bot] balance (after mined): ${afterMindedBalance}`, 'color:green');
+    document.getElementById("btn-mine").disabled = false
 }
 
 }
