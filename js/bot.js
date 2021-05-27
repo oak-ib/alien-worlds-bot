0d59376f964bd962e633481b337ad9a902af6cee
Oak Isarapong <ozaaaaa@gmail.com>
2021-05-27 13:22:54 +0700

    change url wax#2

--- js/bot.js
+++ js/bot.js
@@ -226,7 +226,7 @@ async mine(userAccount){
           transaction_id = result.transaction_id;
         });
 
-        const claimBounty = await getBountyFromTx(transaction_id, userAccount, ["https://wax.pink.gg","https://wax.eosrio.io"])        
+        const claimBounty = await getBountyFromTx(transaction_id, userAccount, ["https://api.waxsweden.org","https://wax.eosrio.io"])        
         this.appendMessage(claimBounty.toString(),'2')
         this.firstMine = false;
         this.previousMineDone = true;
