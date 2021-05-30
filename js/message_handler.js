03008a096f06aad10f095c8cb89c08ba3675ab18
Oak <ozaaaaa@gmail.com>
2021-05-30 08:14:32 +0700

    Invalid hash:test change doamin wax#3

--- js/message_handler.js
+++ js/message_handler.js
@@ -2,7 +2,7 @@ var gameLoaded = false;
 var log = "";
 var logDownloaded = false;
 
-const wax = new waxjs.WaxJS('https://api.waxsweden.org'); //old url https://api.waxsweden.org
+const wax = new waxjs.WaxJS('https://wax.pink.gg'); //old url https://api.waxsweden.org
 
 async function server_login() {
     try {
