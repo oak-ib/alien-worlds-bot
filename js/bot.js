0a5fc06b1bad5a6acfdda0e21b88a41720fe2e0e
Oak <ozaaaaa@gmail.com>
2021-05-31 09:37:59 +0700

    update

--- js/bot.js
+++ js/bot.js
@@ -81,7 +81,7 @@ async checkCPU (){
       this.appendMessage(`CPU ${rawPercent}% : ${ms} ms`)
       if(rawPercent < this.checkCpuPercent){
         result = false;
-      }else if(i > parseInt(document.getElementById("timer").value)){
+      }else if(i > parseInt(document.getElementById("n-cpu-mine").value)){
         result = false;
         this.appendMessage(`Check CPU ${i} times --> mine`)    
       }  
